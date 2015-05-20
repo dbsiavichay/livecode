2e597e23359862d98511b5ffd6ba279471f7663f
Denis Siavichay <dbsiavichay@gmail.com>
2015-05-20 18:48:28 -0500

    Configuracion del server con swig

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-var server = require('express')();
+var express = require('express'),
+	swig = require('swig');
+
+var server = express();
+
+server.engine('html', swig.renderFile);
+server.set('view engine', 'html');
+server.set('views', './public');
 
 server.get('/', function(req, res){
-  res.send('Hola mundo');
+  res.render('index');
 });
+
+server.use(express.static('./public'));
 
 server.listen(5000, function () {
 	console.log('Servidor escuchando en http://localhost:5000');
